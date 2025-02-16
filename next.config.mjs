32a818e0e9d34d3f2f99faf371855502939c4361
asif123raja <121823326+asif123raja@users.noreply.github.com>
2025-02-17 03:58:13 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//     images: {
-//       domains: [ "global.dastgyr.com","images.squarespace-cdn.com","devolas.com.au","www.bigbasket.com" ,"www.fruitsmith.com","www.specialfruit.com","images-prod.healthline.com","www.fitterfly.com","images.immediate.co.uk","www.onceuponachef.com","www.liveorganic.co.in", "www.health.com","nativeindianorganics.com" ,"encrypted-tbn0.gstatic.com", "www.thespruceeats.com","aramgarhorchards.com","blog.lexmed.com","www.veggycation.com.au","upload.wikimedia.org","www.spotlessfruits.com","cdn.shopaccino.com","www.robinage.com","impro.usercontent.one","seed2plant.in","images.everydayhealth.com","nutritionfacts.org","luluindia-prod-assets.s3.ap-south-1.amazonaws.com","www.google.com","res.cloudinary.com",'example.com','resize.indiatvnews.com','www.shutterstock.com','organicbazar.net','t4.ftcdn.net',"images.moneycontrol.com","freerangestock.com","www.jiomart.com","domf5oio6qrcr.cloudfront.net","d3awvtnmmsvyot.cloudfront.net","m.media-amazon.com","www.allthatgrows.in","rukminim2.flixcart.com","organicmandya.com"], // Allow images from this domain
-//     },
+    // images: {
+    //   domains: [ "global.dastgyr.com","images.squarespace-cdn.com","devolas.com.au","www.bigbasket.com" ,"www.fruitsmith.com","www.specialfruit.com","images-prod.healthline.com","www.fitterfly.com","images.immediate.co.uk","www.onceuponachef.com","www.liveorganic.co.in", "www.health.com","nativeindianorganics.com" ,"encrypted-tbn0.gstatic.com", "www.thespruceeats.com","aramgarhorchards.com","blog.lexmed.com","www.veggycation.com.au","upload.wikimedia.org","www.spotlessfruits.com","cdn.shopaccino.com","www.robinage.com","impro.usercontent.one","seed2plant.in","images.everydayhealth.com","nutritionfacts.org","luluindia-prod-assets.s3.ap-south-1.amazonaws.com","www.google.com","res.cloudinary.com",'example.com','resize.indiatvnews.com','www.shutterstock.com','organicbazar.net','t4.ftcdn.net',"images.moneycontrol.com","freerangestock.com","www.jiomart.com","domf5oio6qrcr.cloudfront.net","d3awvtnmmsvyot.cloudfront.net","m.media-amazon.com","www.allthatgrows.in","rukminim2.flixcart.com","organicmandya.com"], // Allow images from this domain
+    // },
 //   };
   
 //   export default nextConfig;
@@ -14,13 +14,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**', // Allow images from all domains
-      },
-    ],
-  },
+      domains: [ "global.dastgyr.com","images.squarespace-cdn.com","devolas.com.au","www.bigbasket.com" ,"www.fruitsmith.com","www.specialfruit.com","images-prod.healthline.com","www.fitterfly.com","images.immediate.co.uk","www.onceuponachef.com","www.liveorganic.co.in", "www.health.com","nativeindianorganics.com" ,"encrypted-tbn0.gstatic.com", "www.thespruceeats.com","aramgarhorchards.com","blog.lexmed.com","www.veggycation.com.au","upload.wikimedia.org","www.spotlessfruits.com","cdn.shopaccino.com","www.robinage.com","impro.usercontent.one","seed2plant.in","images.everydayhealth.com","nutritionfacts.org","luluindia-prod-assets.s3.ap-south-1.amazonaws.com","www.google.com","res.cloudinary.com",'example.com','resize.indiatvnews.com','www.shutterstock.com','organicbazar.net','t4.ftcdn.net',"images.moneycontrol.com","freerangestock.com","www.jiomart.com","domf5oio6qrcr.cloudfront.net","d3awvtnmmsvyot.cloudfront.net","m.media-amazon.com","www.allthatgrows.in","rukminim2.flixcart.com","organicmandya.com"], // Allow images from this domain
+    },
   experimental: {
     forceSwcTransforms: true,
   },
