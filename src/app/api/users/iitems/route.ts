4dd79f42313463f2979f34487a939d1605f0d7f8
asif123raja <121823326+asif123raja@users.noreply.github.com>
2025-02-17 21:22:37 +0530

    Update route.ts

--- src/app/api/users/iitems/route.ts
+++ src/app/api/users/iitems/route.ts
@@ -4,17 +4,17 @@ import { NextRequest, NextResponse } from 'next/server';
 
 connect();
 
-export async function GET(request) {
+export async function GET(request: NextRequest) {
   try {
-    // Fetch all items with only the necessary fields: ID, name, and any other details for the UI
-    const items = await Product.find({}, 'id name price stock_quantity'); // Adjust fields as necessary
+    // Fetch all items with necessary fields
+    const items = await Product.find({}, 'id name price stock_quantity');
 
     return NextResponse.json({
       message: "Items fetched successfully",
       success: true,
       items,
     });
-  } catch (error) {
+  } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
