10f801db5df7871cc2148b34139c5767b10b6df6
asif123raja <121823326+asif123raja@users.noreply.github.com>
2025-02-19 02:14:31 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-{/*         <link rel="icon" href="/favicon.ico" /> {/* Add your favicon here */} */}
+{/*         <link rel="icon" href="/favicon.ico" />
+        {/* Add your favicon here */} */}
         <title>{String(metadata.title)}</title>
         <meta name="description" content={String(metadata.description)} />
       </head>
